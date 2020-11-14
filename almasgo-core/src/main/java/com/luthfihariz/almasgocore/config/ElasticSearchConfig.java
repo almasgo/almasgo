24f3bdbedfb67775fd0c537e40c3d743bb93edd5
luthfihariz <luthfihariz@gmail.com>
2020-11-14 22:05:19 +0700

    Merge pull request #1 from almasgo/feature/multiple-engine
    move get loggedIn user to seperated function

--- almasgo-core/src/main/java/com/luthfihariz/almasgocore/config/ElasticSearchConfig.java
+++ almasgo-core/src/main/java/com/luthfihariz/almasgocore/config/ElasticSearchConfig.java
@@ -1,6 +1,8 @@
 package com.luthfihariz.almasgocore.config;
 
+import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -11,10 +13,13 @@ import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 @Configuration
 public class ElasticSearchConfig {
 
+    @Value("${spring.data.elasticsearch.cluster-nodes}")
+    String[] elasticHost;
+
     @Bean
     public RestHighLevelClient client() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9201", "localhost:9202", "localhost:9203")
+                .connectedTo(elasticHost)
                 .build();
 
         return RestClients.create(clientConfiguration)
