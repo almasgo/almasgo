01c160ae4b33deb1dc647f1a2718fe96786adbe4
bayuars11 <bayuarifsaputro@gmail.com>
2020-11-21 18:47:43 +0700

    change password -> edit file

--- almasgo-core/src/main/java/com/luthfihariz/almasgocore/controller/dto/request/ChangePasswordRequestDto.java
+++ almasgo-core/src/main/java/com/luthfihariz/almasgocore/controller/dto/request/ChangePasswordRequestDto.java
@@ -3,14 +3,11 @@ package com.luthfihariz.almasgocore.controller.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.io.Serializable;
 
 @AllArgsConstructor
 @Getter
 @NoArgsConstructor
-public class ChangePasswordRequestDto{
+public class ChangePasswordRequestDto {
     private String oldPassword;
     private String newPassword;
 }
--- almasgo-core/src/main/java/com/luthfihariz/almasgocore/model/User.java
+++ almasgo-core/src/main/java/com/luthfihariz/almasgocore/model/User.java
@@ -1,12 +1,14 @@
 package com.luthfihariz.almasgocore.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Entity
 @Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue
